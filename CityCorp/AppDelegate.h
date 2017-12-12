ab9b99349c427da7212103ba11abe3e5e909a2db
Dino <dino5050@hotmail.com>
2017-12-12 18:38:31 -0500

    Initial Commit

---
+++ CityCorp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityCorp
+//
+//  Created by Dino Martinez on 12/12/17.
+//  Copyright © 2017 Nezennin Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
