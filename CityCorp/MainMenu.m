f92d65642c44c93e167f8967f14e3cf2cf1da708
Dino <dino5050@hotmail.com>
2018-02-03 11:55:01 -0500

    0.222

--- CityCorp/MainMenu.m
+++ CityCorp/MainMenu.m
@@ -128,8 +128,61 @@ NSUserDefaults *preferences3;
 }
 -(void)create_corp{
     [[panel subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    Button* back = [[Button alloc] init];
+    back.name = @"back";
+    [panel addSubview:[back button2: CGRectMake(panel.frame.size.width-60-5, panel.frame.size.height-50-5, 60, 50.0)]];
 }
-
+-(void)back{
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    CGSize screenSize = screenBound.size;
+    
+    Button* changeProfession = [[Button alloc] init];
+    changeProfession.name = @"change_profession";
+    [panel addSubview:[changeProfession button2: CGRectMake(5, panel.frame.size.height-50-5, 180, 50.0)]];
+    
+    Button* help = [[Button alloc] init];
+    help.name = @"help";
+    [panel addSubview:[help button2: CGRectMake(panel.frame.size.width-60-5, panel.frame.size.height-50-5, 60, 50.0)]];
+    
+    //    make join corp/create corp/go to corp button
+    
+    //    try to make prefences variable global
+    
+    preferences3 = [NSUserDefaults standardUserDefaults];
+    
+    UITextView *info = [[UITextView alloc] init];
+    info.font = [UIFont fontWithName:@"Arial" size:14];
+    info.frame = CGRectMake(5, 5, 200, 150);
+    
+    NSString *username = [preferences3 stringForKey:@"username"];
+    Functions *get_char_info = [[Functions alloc] init];
+    NSString *char_info = [get_char_info httprequest:@"name" :username :@"get_char_info.php"];
+    NSArray *values = [ char_info componentsSeparatedByString: @","];
+    info.text = [[NSString alloc] initWithFormat: @"Name: %@\nLevel: %@\nProfession: %@\nStock Value: %@\nCorporation: %@\nDate Joined: %@\nSkill Points: %@", values[0], values[1], values[2], values[3], values[4], values[5], values[6]];
+    [info setTextColor:[UIColor colorWithRed:255 green:255 blue:255 alpha:255]];
+    [info setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:255]];
+    [panel addSubview:info];
+    
+    UITextView *message = [[UITextView alloc] init];
+    message.font = [UIFont fontWithName:@"Arial" size:16];
+    message.frame = CGRectMake(5, 5+150, 200, 100);
+    message.text = @"Message of the day";
+    [message setTextColor:[UIColor colorWithRed:255 green:255 blue:255 alpha:255]];
+    [message setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:255]];
+    [panel addSubview:message];
+    
+    if([values[2] isEqualToString:@"Corporate"]) [preferences3 setObject:@"corporate" forKey:@"profession"];
+    else if([values[2] isEqualToString:@"Researcher"]) [preferences3 setObject:@"researcher" forKey:@"profession"];
+    else if ([values[2] isEqualToString:@"Hacker"]) [preferences3 setObject:@"hacker" forKey:@"profession"];
+    else if ([values[2] isEqualToString:@"Constructor"]) [preferences3 setObject:@"constructor" forKey:@"profession"];
+    
+    Button* corp = [[Button alloc] init];
+    if([[preferences3 stringForKey:@"profession"] isEqualToString:@"corporate"] && [preferences3 integerForKey:@"hasCorp"] == 1) corp.name = @"goto_corp";
+    else if([[preferences3 stringForKey:@"profession"] isEqualToString:@"corporate"]) corp.name = @"create_corp";
+    else if([preferences3 integerForKey:@"hasCorp"] == 1) corp.name = @"goto_corp";
+    else corp.name = @"join_corp";
+    [panel addSubview:[corp button: CGRectMake(5, panel.frame.size.height-50-5-65, 180, 50.0)]];
+}
 - (void)addBannerViewToView:(UIView *)bannerView {
     bannerView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:bannerView];
